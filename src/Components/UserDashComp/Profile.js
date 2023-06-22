dcd001cfcd92db54bf8dcdcb8f64fa80ecf2cb8c
Noumandurrani <k.durrani003@gmail.com>
2023-06-22 13:16:40 +0500

    upload-profile

--- src/Components/UserDashComp/Profile.js
+++ src/Components/UserDashComp/Profile.js
@@ -23,6 +23,28 @@ function Profile() {
         console.log(err);
       });
   }, []);
+  // set profile
+  const [profile, setProfile] = useState();
+  const handleSetProfile = (e) => {
+    // e.preventDefault();
+    const config = {
+      headers: { "content-type": "multipart/form-data" },
+    };
+    const formData = new FormData();
+    formData.append("profile", profile);
+    axios
+      .post(
+        "http://127.0.0.1:4000/api/project/update/user/profile/" + userId,
+        formData
+      )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  //
   return (
     <div style={{}}>
       <h2>Profile</h2>
@@ -30,10 +52,25 @@ function Profile() {
       {/* <h4>{userData.email}</h4> */}
       {/* <h5>{userId}</h5> */}
       <img
-        src={"http://127.0.0.1:4000/" + userData.profile}
+        src={"http://127.0.0.1:4000/" + userData.profile} //"http://127.0.0.1:4000/" + userData.profile
         alt="dp"
         style={{ height: "150px", borderRadius: "100px", width: "150px" }}
       ></img>
+      <form onSubmit={handleSetProfile}>
+        <input
+          type="file"
+          name="profile"
+          onChange={(e) => {
+            setProfile(e.target.files[0]);
+          }}
+        ></input>
+        <input
+          className="btn btn-light border-dark"
+          value="Save"
+          type="submit"
+        ></input>
+      </form>
+
       <h2>{userData.firstName + " " + userData.lastName}</h2>
       {/* <h2>{userData._id}</h2> */}
       {/* <h2>{userData.email}</h2> */}
